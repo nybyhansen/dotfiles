7957c089a8a66cdcdf06ab48a65213875563bc65
Dennis Hansen <dnus@novonordisk.com>
2025-07-30 11:20:08 +0200

    fix

--- init.sh
+++ init.sh
@@ -2,12 +2,12 @@ echo "installing brew.."
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo "installing from brewfile.."
+brew bundle --global
+
 echo "installing oh-my-zsh.."
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "installing zsh-syntax-highlighting plugin"
-echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 echo "crating symlinks.."
 stow .
